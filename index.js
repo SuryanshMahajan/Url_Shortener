a9b5de9f4e50a3b93e500ec961f306c418f21875
Suryansh0605 <suryanshmahajan0605@gmail.com>
2025-11-25 03:53:13 +0530

    jwt authentication

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const express = require("express");
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require('./routes/url');
-const bodyParser = require("body-parser");
+const {restrictToLoggedinUserOnly, checkAuth} = require('./middleware/auth');
+
 const URL = require('./models/url');
+
+const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user')
+
 const app = express();
 const PORT = 9000;
+require('dotenv').config();
 
-connectToMongoDB('mongodb://localhost:27017/short-url')
-.then(() => console.log('Mongodb Connected'));
+const DATABASE_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+connectToMongoDB(DATABASE_CONNECTION_STRING)
+    .then(() => console.log('Mongodb Connected'));
+
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
-app.use('/url', urlRoute);
+app.use('/url',restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
+app.use("/",checkAuth, staticRoute);
 
-app.get('/:shortId', async(req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
-    }, { $push: {
-        visitHistory: {
-            timestamp: Date.now(),
+    }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            },
         },
-    },
-}
-);
-res.redirect(entry.redirectURL)
+    }
+    );
+    res.redirect(entry.redirectURL)
 });
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
